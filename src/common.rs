af9bbb007c7bfd458fcf31f9a27d6c321f77881f
nyanmStriking <nyanm2k@gmail.com>
2026-06-07 01:20:06 +0900

    complete special tasks

--- src/common.rs
+++ src/common.rs
@@ -69,19 +69,24 @@ pub const SPECIAL_IDS: &[u32] = &[26, 709, 822, 927, 1148, 1225, 1758];
 
 // explicit per-output rows for the multi-audio specials; shared fields (artist/bpm/date/version) come from the db by id
 pub const SPECIAL_TASKS: &[SpecialTask] = &[
+    SpecialTask { id: 26,   audio_token: "",   jacket_slot: 3, title: "" },
+    SpecialTask { id: 26,   audio_token: "4i", jacket_slot: 4, title: "ごりらがいるんだ - Infinity Edit - " },
     SpecialTask { id: 709,  audio_token: "",   jacket_slot: 3, title: "" },
-    SpecialTask { id: 709,  audio_token: "4i", jacket_slot: 4, title: "月光乱舞 - Gravity Edit. - " },
+    SpecialTask { id: 709,  audio_token: "4i", jacket_slot: 4, title: "月光乱舞 - Gravity Edit - " },
+    SpecialTask { id: 822,  audio_token: "",   jacket_slot: 1, title: "" },
+    SpecialTask { id: 822,  audio_token: "4i", jacket_slot: 4, title: "Help me, ERINNNNNN!! - SH Style Gravity Edit -" },
     SpecialTask { id: 927,  audio_token: "",   jacket_slot: 1, title: "" },
     SpecialTask { id: 927,  audio_token: "4i", jacket_slot: 4, title: "極圏 - Heavenly Edit - " },
     SpecialTask { id: 1148, audio_token: "1n", jacket_slot: 1, title: "TWO-TORIAL - Novice Edit - " },
     SpecialTask { id: 1148, audio_token: "2a", jacket_slot: 2, title: "TWO-TORIAL - Advance Edit - " },
     SpecialTask { id: 1148, audio_token: "3e", jacket_slot: 3, title: "TWO-TORIAL - Exhaust Edit - " },
     SpecialTask { id: 1148, audio_token: "5m", jacket_slot: 5, title: "TWO-TORIAL - Maximum Edit - " },
+    SpecialTask { id: 1225, audio_token: "",   jacket_slot: 1, title: "" },
+    SpecialTask { id: 1225, audio_token: "5m", jacket_slot: 1, title: "Evans - Maximum Edit - " },
     SpecialTask { id: 1758, audio_token: "1n", jacket_slot: 1, title: "Help me, ERINNNNNN!! #幻想郷ホロイズムver. - Pekora Usada, Miko Sakura, Shion Murasaki Edit - " },
     SpecialTask { id: 1758, audio_token: "2a", jacket_slot: 2, title: "Help me, ERINNNNNN!! #幻想郷ホロイズムver. - Marine Houshou, Fubuki Shirakami, Rushia Uruha Edit - " },
     SpecialTask { id: 1758, audio_token: "3e", jacket_slot: 3, title: "Help me, ERINNNNNN!! #幻想郷ホロイズムver. - Marine Houshou, Matsuri Natsuiro, Aqua Minato Edit - " },
     SpecialTask { id: 1758, audio_token: "5m", jacket_slot: 5, title: "Help me, ERINNNNNN!! #幻想郷ホロイズムver. - Noel Shirogane, Flare Shiranui Edit - " },
-    // TODO awaiting edit titles: 26 (ごりらがいるんだ, _4i), 822 (Help me ERINNNNNN -SH Style-, _4i), 1225 (Evans, _5m)
 ];
 
 // safe accessor: out-of-range versions fall back to an empty string instead of panicking
